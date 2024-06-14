ee7ccb856e1a6f292df9658622dd697a483fef30
azamov-asadbek271 <azamjonovasa45@gmail.com>
2024-06-14 23:27:39 +0300

    ssssssssdsdafasfa asfasd

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
           autoPlay
           loop
           muted
-          className="max-h-full max-w-full object-cover"
+          className="h-full max-w-full object-cover"
         />
         <Form className=" flex flex-col gap-y-2 absolute" method="post">
           <div className="card  p-8 bg-none items-center  ">
