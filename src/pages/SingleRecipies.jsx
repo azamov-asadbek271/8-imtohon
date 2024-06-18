a814a1745e528c8d8afafa30a2121aef0cd338fc
azamov-asadbek271 <azamjonovasa45@gmail.com>
2024-06-18 13:38:35 +0300

    input

--- src/pages/SingleRecipies.jsx
+++ src/pages/SingleRecipies.jsx
@@ -1,18 +1,20 @@
-
 import { db } from "../firebase/FirebaseConfig";
 import { doc, getDoc } from "firebase/firestore";
-
 import { useLoaderData } from "react-router-dom";
- export const loader = async ({ params }) => {
-   const docRef = doc(db, "foods", params.id);
-   const docSnap = await getDoc(docRef);
 
-   if (docSnap.exists()) {
-     return docSnap.data();
-   } else {
-     // docSnap.data() will be undefined in this case
-     console.log("No such document!");
-   }
+ export const loader = async ({ params }) => {
+  console.log(params.id);
+   const docRef = doc(db, "foods", params.id);
+   console.log(docRef);
+   const docSnap = await getDoc(docRef);
+   console.log(docSnap.exists());
+
+  //  if (docSnap.exists()) {
+  //    return docSnap.data();
+  //  } else {
+  //    // docSnap.data() will be undefined in this case
+  //    console.log("No such document!");
+  //  }
    return null;
  };
 
