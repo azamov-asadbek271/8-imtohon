0ae2c08df6b3c9f69a00d52c803dd8ef79468e8a
azamov-asadbek271 <azamjonovasa45@gmail.com>
2024-06-20 10:10:56 +0300

    order Total

--- src/pages/Shopping.jsx
+++ src/pages/Shopping.jsx
@@ -1,17 +1,21 @@
 import { useSelector } from "react-redux"
 
 function Shopping() {
-    const { cartItem } = useSelector((state) => state.cartState);
+    const { cartItem, orderTotal } = useSelector((state) => state.cartState);
     console.log(cartItem);
     const { title, price, ingredients, image, amount, cookingTime } =
       cartItem[0];
   return (
     <>
       {cartItem && (
-        <div className="mt-10">
-          <div className="w-96">
+        <div className="mt-10 flex justify-between">
+          <div className="w-96 card">
             <h2 className="font-bold text-3xl text-center">{title}</h2>
-            <img src={image} alt="" className="w-full object-cover mt-5" />
+            <img
+              src={image}
+              alt=""
+              className="w-full object-cover mt-5 rounded"
+            />
             <div className="flex gap-5 mt-5">
               <p>ingredients: </p>
               {ingredients.map((item) => {
@@ -37,6 +41,17 @@ function Shopping() {
               <span className="font-medium">Amount:</span>
               {amount}
             </p>
+          </div>
+          <div className="mt-20">
+            <p className=" flex justify-between text-xl border-b border-base-300 pb-2 w-96 mt-10">
+              <span className="font-bold">Subtotal</span>
+              <span className="font-medium">{price},000 So'm</span>
+            </p>
+            <p className=" flex justify-between text-xl border-b border-base-300 pb-2 w-96 mt-10">
+              <span className="font-bold">Order total</span>
+              <span className="font-medium">{orderTotal},000 So'm</span>
+            </p>
+            <button className="btn btn-primary mt-5 w-full">Check Out</button>
           </div>
         </div>
       )}
